ecba9a03d5fe7ecfb4dd5f5ff8aab3c6eb37ef9f
hamdanzd <143369477+hamdanzd@users.noreply.github.com>
2023-10-15 22:22:06 -0400

    Update lab5.py

--- Labs/lab5.py
+++ Labs/lab5.py
@@ -1,1 +1,13 @@
+from datetime import datetime
 
+year = int(input("Enter your birth year (YYYY): "))
+month = int(input("Enter your birth month (MM): "))
+day = int(input("Enter your birth day (DD): "))
+
+current_date = datetime.now()
+
+birthdate = datetime(year, month, day)
+
+age_in_seconds = (current_date - birthdate).total_seconds()
+
+print(f"You are approximately {int(age_in_seconds)} seconds old.")
