dfa67f70ba96ba62a01b8286998b4ab2609dd695
hamdanzd <143369477+hamdanzd@users.noreply.github.com>
2023-11-05 23:27:54 -0500

    Update req.py

--- lab9/req.py
+++ lab9/req.py
@@ -1,1 +1,9 @@
+import requests
+
+api_url = "http://localhost:3000/info"
+response = requests.get(api_url)
+data = response.json()
+print("Retrieved widget information:")
+for widget in data:
+    print(f"Widget: {widget['name']}, Color: {widget['color']}")
 
